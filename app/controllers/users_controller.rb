9700d2b07155bb2f8d5b3eb985aa586b6f9a760d
Alex Zdatny <alexzdatny@gmail.com>
2018-08-07 03:57:05 +0000

    built post login and require flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
+require 'rack-flash'
 class UsersController < ApplicationController
+
+  use Rack::Flash
 
   get '/signup' do
     erb :'users/create_user'
@@ -6,11 +9,17 @@ class UsersController < ApplicationController
 
   post '/signup' do
   end
-  
+
   get '/login' do
     erb :'/users/login'
   end
 
   post '/login' do
+    user = User.find_By(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/itineraries'
+    else
+      redirect '/login'
   end
 end

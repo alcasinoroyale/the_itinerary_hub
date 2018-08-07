d3096983b4279339a3c5530ab93f31c530a0f086
Alex Zdatny <alexzdatny@gmail.com>
2018-08-07 04:00:54 +0000

    updating elements of the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,36 @@ class UsersController < ApplicationController
   use Rack::Flash
 
   get '/signup' do
-    erb :'users/create_user'
+    if logged_in?
+      redirect 'itineraries'
+    else
+      erb :'users/create_user'
+    end
   end
 
   post '/signup' do
+    user = User.new(params)
+    if user.save
+      session[:user_id] = user.id
+      redirect '/itineraries'
+    else
+      redirect '/'
+    end
   end
 
   get '/login' do
-    erb :'/users/login'
+    if logged_in?
+      redirect '/itineraries'
+    else
+      erb :'/users/login'
+    end
   end
 
   post '/login' do
     user = User.find_By(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      @user = current_user
       redirect '/itineraries'
     else
       redirect '/login'

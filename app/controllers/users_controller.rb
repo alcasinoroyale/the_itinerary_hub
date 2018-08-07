43060bd217b01e936269224f7855db26763f9df1
Alex Zdatny <alexzdatny@gmail.com>
2018-08-07 03:54:59 +0000

    starting on signup and login methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+  end
+  
   get '/login' do
     erb :'/users/login'
+  end
+
+  post '/login' do
   end
 end

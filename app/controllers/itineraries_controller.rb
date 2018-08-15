669d32d3612575430c6d9078ec0af18735acc007
Alex Zdatny <alexzdatny@gmail.com>
2018-08-15 21:32:52 +0000

    building methods in the itineraries_controller

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,2 +1,19 @@
 class ItinerariesController < ApplicationController
+  get '/itineraries' do #reading all of the itineraries
+      if logged_in?
+        @user = current_user
+        @itineraries = Itinerary.all
+        erb :'/itineraries/index'
+      else
+        redirect to '/login'
+    end
+  end
+
+  get '/itineraries/new' do #create an Itinerary
+      if logged_in?
+        erb :'itineraries/new'
+      else
+        redirect to '/login'
+    end
+  end
 end

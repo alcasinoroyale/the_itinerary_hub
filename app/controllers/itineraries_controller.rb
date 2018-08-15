475178cb91c1239862682c6e70f02fd631cf5017
Alex Zdatny <alexzdatny@gmail.com>
2018-08-15 21:38:43 +0000

    continued to build methods in itineraries controller

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -16,4 +16,28 @@ class ItinerariesController < ApplicationController
         redirect to '/login'
     end
   end
+
+  post '/itineraries' do
+    if params[:itinerary] == ""
+      erb :'itineraries/new'
+    else
+      @itinerary = current_user.itinerary.build(params)
+      @itinerary.user = current_user
+      @itinerary.save
+      redirect to "/itineraries/#{@itinerary.id}"
+    end
+  end
+
+  get '/itineraries/:id' do
+    if logged_in?
+      if @itinerary = current_user.itinerary.find_by(id: params[:id])
+        erb :'itineraries/edit'
+      else
+        flash[:message] = "You only have the ability to edit your itineraries."
+        redirect to '/itineraries'
+      end
+    else
+      redirect to '/login'
+    end
+  end
 end

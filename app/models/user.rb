28708524b462a858a88c3341389e27f7aca52392
alcasinoroyale <alexzdatny@gmail.com>
2019-10-22 16:08:37 -0400

    Fix formating and convert users list to table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ActiveRecord::Base
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
 
-# add instance method called trip_count that returns the number of itineraries that a user has and then where you're display a username's name, add trip count after
-# add class method called most_traveled, return user.trip_count, display on home page
-
   def self.most_traveled
     self.all.max_by do |user|
       user.trip_count
